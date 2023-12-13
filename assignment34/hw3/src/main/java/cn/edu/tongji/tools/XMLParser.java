f01c8a6182bf73c131d9aef44c6c99fce4eccba5
TangciuYueng <2310360939@qq.com>
2023-12-14 00:03:50 +0800

    readLine & save & load

--- assignment34/hw3/src/main/java/cn/edu/tongji/tools/XMLParser.java
+++ assignment34/hw3/src/main/java/cn/edu/tongji/tools/XMLParser.java
@@ -7,18 +7,31 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class XMLParser {
 
     public static void main(String[] args) {
         String xmlFilePath = "./test.xml";
-        HashMap<String, HashMap<Integer, Integer>> authorData = parseXML(xmlFilePath);
-        printAuthorData(authorData);
+        // 解析 xml 建立 author 到 year 到 publications 的映射的映射
+//        HashMap<String, HashMap<Integer, Integer>> authorData = parseXMLWithWholeFile(xmlFilePath);
+        HashMap<String, HashMap<Integer, Integer>> authorData = parseXMLWithLine(new File(xmlFilePath));
+        // 打印信息
+        printData(authorData);
+        // 保存文件
+        saveToFile(authorData, "data.ser");
+        // 读取文件
+        var temp = loadFromFile("data.ser");
+        printData(temp);
     }
 
-    public static HashMap<String, HashMap<Integer, Integer>> parseXML(String xmlFilePath) {
+    // 解析 xml 获取其中我们需要的信息
+    public static HashMap<String, HashMap<Integer, Integer>> parseXMLWithWholeFile(String xmlFilePath) {
         HashMap<String, HashMap<Integer, Integer>> authorData = new HashMap<>();
 
         try {
@@ -39,6 +52,45 @@ public class XMLParser {
         }
 
         return authorData;
+    }
+
+    private static HashMap<String, HashMap<Integer, Integer>> parseXMLWithLine(File file) {
+        HashMap<String, HashMap<Integer, Integer>> res = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            System.out.println(file.getName() + " start");
+            String line;
+            List<String> authors = new ArrayList<>();
+
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("<article") || line.contains("<book")) {
+                    authors.clear();
+                }
+
+                if (line.contains("<author")) {
+                    Matcher matcher = Pattern.compile("<author.*?>(.*?)</author>").matcher(line);
+                    while (matcher.find()) {
+                        String author = matcher.group(1);
+                        authors.add(author);
+                    }
+                }
+
+                if (line.contains("<year>")) {
+                    Matcher matcher = Pattern.compile("<year>(\\d+)</year>").matcher(line);
+                    while (matcher.find()) {
+                        int year = Integer.parseInt(matcher.group(1));
+                        for (String author : authors) {
+                            res.computeIfAbsent(author, k -> new HashMap<>())
+                                    .merge(year, 1, Integer::sum);
+                        }
+                    }
+                }
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
     private static void processNodeList(NodeList nodeList, HashMap<String, HashMap<Integer, Integer>> authorData) {
@@ -69,7 +121,7 @@ public class XMLParser {
         }
     }
 
-    private static void printAuthorData(HashMap<String, HashMap<Integer, Integer>> authorData) {
+    private static void printData(HashMap<String, HashMap<Integer, Integer>> authorData) {
         for (String author : authorData.keySet()) {
             System.out.println("Author: " + author);
             HashMap<Integer, Integer> yearData = authorData.get(author);
@@ -79,6 +131,24 @@ public class XMLParser {
             }
             System.out.println();
         }
+    }
+
+    private static void saveToFile(HashMap<String, HashMap<Integer, Integer>> data, String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(data);
+            System.out.println("Data saved to " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HashMap<String, HashMap<Integer, Integer>> loadFromFile(String fileName) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            return (HashMap<String, HashMap<Integer, Integer>>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new HashMap<>();
     }
 }
 
