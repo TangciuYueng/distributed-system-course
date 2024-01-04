4e8b13efef7292cf4b76417d4defce7d85dbf59c
TangciuYueng <2310360939@qq.com>
2024-01-04 13:45:33 +0800

    update server input args copy num

--- assignment34/hw3/src/main/java/cn/edu/tongji/server/MainServer.java
+++ assignment34/hw3/src/main/java/cn/edu/tongji/server/MainServer.java
@@ -9,17 +9,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainServer {
-    public static final int COPY_NUM = 4;
-    public static final int BUCKET_PER_SERVER = 4;
-    public static final int PORT = 9999;
+    private static final String FILE_PREFIX = "dblp_line_processed_chunk_";
+    private static final String FILE_POSTFIX = "_index_tree.ser";
+    private static final int BUCKET_PER_SERVER = 4;
+    private static final int PORT = 9999;
 
     public static void createServerThread(int serverNum, int port) {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             PersistentBTree<String, Long>[] trees = new PersistentBTree[BUCKET_PER_SERVER];
 
+            // 前置工作 读取索引
             for (int i = 0; i < BUCKET_PER_SERVER; i++) {
                 final long startTime = System.currentTimeMillis();
-                trees[i] = PersistentBTree.loadFromFile("dblp_line_processed_chunk_" + (serverNum + 1) + "_bucket_" + i + "_index_tree.ser");
+                trees[i] = PersistentBTree.loadFromFile( FILE_PREFIX + (serverNum + 1) + "_bucket_" + i + FILE_POSTFIX);
                 System.out.println("块" + (serverNum + 1) + "读取" + i + "号桶索引用时：" + ((double) (System.currentTimeMillis() - startTime) / 1000) + "s");
             }
 
@@ -40,19 +42,20 @@ public class MainServer {
     }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("need server num");
+        if (args.length != 2) {
+            System.out.println("need args: <startChunkNum> <copyNum>");
             return;
         }
-        // 一个服务器 4 个文件块 开 4 个线程 负责 9999 - 10002 端口进行处理
+        // 一个服务器 copyNum 个文件块 开 copyNum 个线程 负责 9999 - 10002/3 端口进行处理
         try {
-            int serverNum = Integer.parseInt(args[0]);
-            try (ExecutorService exec = Executors.newCachedThreadPool()) {
-                for (int i = 0; i < COPY_NUM; ++i) {
+            int startChunkNum = Integer.parseInt(args[0]);
+            int copyNum = Integer.parseInt(args[1]);
+            try (ExecutorService exec = Executors.newFixedThreadPool(copyNum)) {
+                for (int i = 0; i < copyNum; ++i) {
                     int finalI = i;
                     int port = PORT + i;
                     exec.execute(() -> {
-                        createServerThread((serverNum + finalI) % COPY_NUM, port);
+                        createServerThread((startChunkNum + finalI) % copyNum, port);
                     });
                 }
             }
